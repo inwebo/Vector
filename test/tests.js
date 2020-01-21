dc6ef6918c593177788b9b46b58d1e1f9c327e2f
inwebo <gh_kvrg1hir!>
2020-01-21 21:27:08 +0100

    Hello world

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,9 @@
-QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 === 1, "Passed!" );
+import Vector2D from "../src/Vector2D";
+var assert = require('assert');
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+    });
 });
