6177ad9c4a286363818eaa12fc1dd4814b99d46d
inwebo <gh_kvrg1hir!>
2020-01-21 21:45:02 +0100

    Add tests

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,105 @@
 import Vector2D from "../src/Vector2D";
-var assert = require('assert');
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal([1, 2, 3].indexOf(4), -1);
+
+const assert = require('assert');
+
+describe('Vector contructor', () => {
+
+    describe('let v = new Vector()', () => {
+        let v = new Vector2D();
+        it('v.getX() = 0', () => {
+            assert.equal(v.getX(), 0);
+        });
+        it('v.getY() = 0', () => {
+            assert.equal(v.getY(), 0);
         });
     });
+
+    describe('let v = new Vector(100, 77)', () => {
+        let v = new Vector2D(100, 77);
+        it('v.getX() = 100', () => {
+            assert.equal(v.getX(), 100);
+        });
+        it('v.getY() = 77', () => {
+            assert.equal(v.getY(), 77);
+        });
+    });
+
+    describe('let v = Vector.fromArray([])', () => {
+        let v = Vector2D.fromArray([]);
+
+        it('v.getX() = 0', () => {
+            assert.equal(v.getX(), 0);
+        });
+        it('v.getY() = 0', () => {
+            assert.equal(v.getY(), 0);
+        });
+    });
+
+    describe('let v = Vector.fromArray([1, 2])', () => {
+        let v = Vector2D.fromArray([1, 2]);
+
+        it('v.getX() = 1', () => {
+            assert.equal(v.getX(), 1);
+        });
+        it('v.getY() = 2', () => {
+            assert.equal(v.getY(), 2);
+        });
+    });
+
+    describe('let v = Vector.fromArray([1, 2, 3])', () => {
+        let v = Vector2D.fromArray([1, 2]);
+
+        it('v.getX() = 1', () => {
+            assert.equal(v.getX(), 1);
+        });
+        it('v.getY() = 2', () => {
+            assert.equal(v.getY(), 2);
+        });
+    });
+
+    describe('let v = Vector.fromObject({})', () => {
+        let v = Vector2D.fromObject({});
+
+        it('v.getX() = 0', () => {
+            assert.equal(v.getX(), 0);
+        });
+        it('v.getY() = 0', () => {
+            assert.equal(v.getY(), 0);
+        });
+    });
+
+    describe('let v = Vector.fromObject({x: 1, y:2})', () => {
+        let v = Vector2D.fromObject({x:1, y:2});
+
+        it('v.getX() = 1', () => {
+            assert.equal(v.getX(), 1);
+        });
+        it('v.getY() = 2', () => {
+            assert.equal(v.getY(), 2);
+        });
+    });
+
+    describe('let v = Vector.fromObject({foo: 1, bar:2})', () => {
+        let v = Vector2D.fromObject({foo:1, bar:2});
+
+        it('v.getX() = 0', () => {
+            assert.equal(v.getX(), 0);
+        });
+        it('v.getY() = 0', () => {
+            assert.equal(v.getY(), 0);
+        });
+    });
+
+    describe('let v = Vector.fromObject({foo: 1, bar:2})', () => {
+        let v = Vector2D.fromObject({foo:1, bar:2});
+
+        it('v.getX() = 0', () => {
+            assert.equal(v.getX(), 0);
+        });
+        it('v.getY() = 0', () => {
+            assert.equal(v.getY(), 0);
+        });
+    });
+
+
 });
