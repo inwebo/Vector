8bbd426c512769d6795dafc6b493713dab748acb
inwebo <gh_kvrg1hir!>
2020-01-22 08:28:08 +0100

    Add try catch

--- demos/demo.js
+++ demos/demo.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded",function() {
+import Vector2D from "../src/Vector2D";
 
+document.addEventListener("DOMContentLoaded",function() {
+    try {
+
+    } catch (e) {
+        console.error(e.stack);
+        console.error(e.fileName);
+        console.error(e.lineNumber);
+        console.error(e.columnNumber);
+    }
 });
