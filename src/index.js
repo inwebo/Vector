487c7d4fd8da38e9e583c5445485cbd44e279020
inwebo <github@inwebo.net>
2020-05-19 19:43:20 +0200

    Update include

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import MathExtension from "./MathExtension";
 import Vector2D from "./Vector2D";
 import Vector2DValidator from "./Vector2DValidator";
+
+export {
+    MathExtension,
+    Vector2D,
+    Vector2DValidator
+};
