debac7e3d88a82b3152f72b6efe2e6ab5cd3bcc4
inwebo <github@inwebo.net>
2021-02-02 19:20:52 +0100

    Fix npm standard fs missing

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    entry: {
-        main: './src/index.js',
-        test: './test/tests.js',
-    },
-    output: {
-        filename: '[name].min.js',
-        path: path.join(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src'),
-                loader: 'babel-loader'
-            },
-        ]
-    }
-};
+  node: {
+    fs: 'empty'
+  },
+  entry: {
+    main: './src/index.js',
+    test: './test/tests.js'
+  },
+  output: {
+    filename: '[name].min.js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
